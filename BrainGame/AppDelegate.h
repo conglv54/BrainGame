7c60fafec9ddb8102b86066bb72ba42bbd345729
Lê Công <conglv@nal.vn>
2014-12-15 16:45:08 +0700

    Initial Commit

---
+++ BrainGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrainGame
+//
+//  Created by Lê Công on 12/15/14.
+//  Copyright (c) 2014 Lê Công. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BrainGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BrainGame
+//
+
+//  Copyright (c) 2014 Lê Công. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
